c5525ae4df0955cb4bd835a8f2cb6d911e8044de
PenuelSam <penuelsamuel54@gmail.com>
2023-09-13 12:14:09 +0100

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -57,7 +57,7 @@ const Hero = () => {
   console.log('top 10',topTenMovies)
 
   return (
-    <div className="container mx-auto p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+    <div className="container mx-auto p-4 grid grid-cols-1 place-items-center sm:grid-cols-3 md:grid-cols-4 gap-4">
       {topTenMovies.map((movie) => (
         <MovieCard
           key={movie.id}
