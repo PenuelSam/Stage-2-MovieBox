8740ef27197d45c97b130b932fe1a3f2a61884de
PenuelSam <penuelsamuel54@gmail.com>
2023-09-13 11:58:22 +0100

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,115 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import logo from "../assets/logo.png";
+import menu from "../assets/Menu.png";
+import imdb from "../assets/IMDB.png";
+import SearchIcon from "@mui/icons-material/Search";
+import berry from "../assets/berry.png";
+import image from "../assets/Button.png";
+
+const Header1 = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <header className="fixed top-0 right-0 left-0 py-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <img src={logo} alt="MovieBox Logo" className="w-32" />
+        <div className="flex relative items-center">
+          <input
+            type="text"
+            placeholder="What do you want to watch?"
+            value={query}
+            onChange={handleInputChange}
+            className=" h-[2rem]  w-[35rem] text-white pl-5 bg-transparent border rounded-md focus:outline-none"
+          />
+          <SearchIcon
+            onClick={handleSearch}
+            sx={{ fill: "white" }}
+            className="absolute right-2 cursor-pointer"
+          />
+        </div>
+        <div className="">
+          <img src={menu} alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Header = () => {
+  const [randomMovie, setRandomMovie] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYmNhYTU2ODEwZjM1Mjk0NTA1MzJkZGJkZDZiYjU4YSIsInN1YiI6IjYzMmNhN2ZmNTU5MzdiMDA3YzA4YWZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rtnf31SPKvcYQpwzFOFm7X_ORuuShkXNSrSD1OonveQ",
+      },
+    };
+
+    // Fetch a random top-rated movie from the API
+    fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const randomIndex = Math.floor(Math.random() * response.results.length);
+        const randomMovieData = response.results[randomIndex];
+        setRandomMovie(randomMovieData);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  const searchMovies = (query) => {
+    setSearchQuery(query);
+    // Fetch movies based on the search query
+    // You can use a similar approach as fetching top-rated movies
+  };
+
+  const backgroundImageStyle = {
+    backgroundImage: `url(https://image.tmdb.org/t/p/original${randomMovie?.backdrop_path})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  };
+
+  return (
+    <header className="h-screen relative" style={backgroundImageStyle}>
+      <Header1 onSearch={searchMovies} search={searchQuery} />
+      <div className=" h-[285px] absolute left-10 top-52 w-[404px] flex flex-col justify-center items-start text-white">
+        <h1 className="text-4xl font-semibold">{randomMovie?.title}</h1>
+        <div className="flex items-center gap-10 pt-3">
+          <div className="flex items-center justify-center gap-2">
+            <div>
+              <img src={imdb} alt="" />
+            </div>
+            <p>{randomMovie?.popularity}</p>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <div>
+              <img src={berry} alt="" />
+            </div>
+            <p>{randomMovie?.genre_ids[1]}%</p>
+          </div>
+        </div>
+        <p className="my-5">{randomMovie?.overview}</p>
+        <div>
+          <img src={image} alt="" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable react/prop-types */
+import  { useState, useEffect } from "react";
+import imdb from "../assets/IMDB.png";
+import berry from "../assets/berry.png";
+
+
+const MovieCard = ({  title, image, popularity, genre_ids }) => (
+  <div className="p-4">
+    
+    <div className="bg-white h-[513px] w-[250px] rounded-lg shadow-lg overflow-hidden">
+      
+      <img src={image} alt={title} className="w-full object-cover" />
+      <div className="p-4">
+        <span className="text-[#b6b5b5]">USA, 2021</span>
+        <h3 className="text-xl font-semibold mb-2">{title}</h3>
+        <div className="flex gap-5">
+        <div className="flex items-center justify-center gap-2">
+            <div>
+              <img src={imdb} alt="" />
+            </div>
+            <p>{popularity} / 100</p>
+          </div>
+          <div className="flex items-center justify-center gap-2">
+            <div>
+              <img src={berry} alt="" />
+            </div>
+            <p>{genre_ids[0]}%</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    
+  </div>
+);
+
+const Hero = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYmNhYTU2ODEwZjM1Mjk0NTA1MzJkZGJkZDZiYjU4YSIsInN1YiI6IjYzMmNhN2ZmNTU5MzdiMDA3YzA4YWZhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rtnf31SPKvcYQpwzFOFm7X_ORuuShkXNSrSD1OonveQ'
+      }
+    };
+
+    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+      .then(response => response.json())
+      .then(response => setMovies(response.results))
+      .catch(err => console.error(err));
+  }, []);
+
+  // Slice the first 10 movies
+  const topTenMovies = movies.slice(0, 10);
+  console.log('top 10',topTenMovies)
+
+  return (
+    <div className="container mx-auto p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      {topTenMovies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          description={movie.overview}
+          popularity={movie.vote_average}
+          genre_ids={movie.genre_ids}
+          id={movie.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Hero;
